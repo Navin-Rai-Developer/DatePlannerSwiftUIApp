9182bb76c0ab4103fa38fdab526080317bcda28d
Navin Rai <rai.navinrai20@gmail.com>
2025-06-01 00:10:09 +0530

    Initial Commit

---
+++ Date Planner SwiftUIApp/Date_Planner_SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Date_Planner_SwiftUIAppApp.swift
+//  Date Planner SwiftUIApp
+//
+//  Created by Navin Rai on 01/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Date_Planner_SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
